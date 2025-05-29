66a8fd0388a2326c57eab2b7739f1589f8805a33
Migssssssssssssss <miguel.eduardo.almeida@escola.pr.gov.br>
2025-05-28 22:26:58 -0300

    save 28/05

--- perguntas.js
+++ perguntas.js
@@ -1,25 +1,23 @@
-criarCartão(
-    'programação,' 
-    'O que é JavaScript?' 
-    'O JavaScript é uma linguagem de programação'
+criaCartao(
+    'Programação', 
+    'O que é Python?', 
+    'O Python é uma linguagem de programação'
 )
-criarCartão(
-    'programação,' 
-    'O que é CSS??' 
-    ' O CSS é uma linguagem de estilização.'
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
 )
-criarCartão(
-    'geografia,' 
-    'qual é a capital da frança?' 
-    'A capital da França é paris'
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
 )
-criarCartão(
-    'programação,' 
-    'o que é função?' 
-    'uma função é um bloco de codigo que executa alguma tarefa'
-)
-criarCartão(
-    'programação,' 
-    'O que é python?' 
-    'A capital da França é paris'
+
+criaCartao (
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação'
 )
